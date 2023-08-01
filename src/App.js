cd542ff475f4387d7b47f99323dc9bda1ac1f5f5
dsl2022 <2015rpro@gmail.com>
2023-07-31 21:12:26 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
-
+import {useState} from "react"
 function App() {
+  const [firstName,setFirstName]=useState("")
+  const [lastName,setLastName]=useState("")
+  const [email,setEmail]=useState("")
+  const [password,setPassword]=useState("")
+
+  function handleSubmit(e){
+    e.preventDefault()
+    console.log({firstName,lastName,email,password})
+  }
+
+  function handleFirstNameChange(e){    
+    setFirstName(e.target.value)
+  }
+  function handleLastNameChange(e){    
+    setLastName(e.target.value)
+  }
+  function handleEmailChange(e){    
+    setEmail(e.target.value)
+  }
+  function handlePasswordChange(e){    
+    setPassword(e.target.value)
+  }
+  console.log({firstName})
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit}>
+        <label>          
+          First Name:
+          <input onChange={handleFirstNameChange} value={firstName} type='text'/>
+        </label> 
+        <label>          
+          Last Name:
+          <input onChange={handleLastNameChange} value={lastName} type='text'/>
+        </label> 
+        <label>          
+          Email:
+          <input onChange={handleEmailChange} value={email} type='text'/>
+        </label> 
+        <label>          
+          Password:
+          <input onChange={handlePasswordChange} value={password} type='text'/>
+        </label> 
+          <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
